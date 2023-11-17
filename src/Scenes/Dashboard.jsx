4ad4b5bbedc24f3a219458cfaa0a582e8bb782e7
Prometheus <www.prometheusvon@gmail.com>
2023-11-18 02:01:46 +0800

    committed as of 11/18 02-01

--- src/Scenes/Dashboard.jsx
+++ src/Scenes/Dashboard.jsx
@@ -2,41 +2,126 @@ import React, { useEffect, useState } from 'react'
 import Header from '../Components/Header'
 import { Box, Typography, useTheme } from '@mui/material'
 import { tokens } from '../theme'
-import { ResponsivePie } from "@nivo/pie"
-import {getDatabase, onValue, ref} from "firebase/database"
+import { ResponsiveBar } from "@nivo/bar"
+import { collection, getDocs, getFirestore, where, doc, getDoc } from '@firebase/firestore'
+import { ResponsivePie } from '@nivo/pie'
 
 const Dashboard = () => {
   const theme = useTheme()
   const colors = tokens(theme.palette.mode);
-  const [allGrades, setAllGrades] = useState([]);
-  const [allStrands, setAllStrands] = useState([])
-  const db = getDatabase();
+  const [allVIP, setAllVIP] = useState("");
+  const [allLB, setAllLB] = useState("");
+  const [allUPB, setAllUPB] = useState("");
 
-  useEffect(() => {
-    const gradeCounts = []
-    const strandCounts = []
-    onValue(ref(db, "Grade Level/"),
-    (snapshot) => {
-      snapshot.forEach((element) =>{
-       gradeCounts.push({id: element.key, value: element.size})
-      })
-      setAllGrades(gradeCounts)
-    })
-    onValue(ref(db, "Strand"),
-    (snapshot) =>{
-      snapshot.forEach((elemento) =>{
-        strandCounts.push({id: elemento.key, value: elemento.size})
-      })
-      setAllStrands(strandCounts)
-    })
+  //for the graph data
+  const [vipPay, setVipPay] = useState([]);
+  const [lbPay, setLbPay] = useState([]);
+  const [upbPay, setUpbPay] = useState([]);
 
-  },[db])
+  const [allSeats, setAllSeats] = useState([]);
+
+  const db = getFirestore();
+
+  useEffect(() => {   
+    const getVip = async () => {
+      const queryVIP = await getDocs(collection(db, "VIP"));
+      setAllVIP(queryVIP.size)
+      
+      const modes = await getDocs(collection(db, "VIP"), where("paymentMode", "!=", ""));
+      const modeCounts = []
+      modes.forEach((snap) => {
+        const data = snap.data();
+        if(data.paymentMode){
+          if(modeCounts[data.paymentMode]){
+            modeCounts[data.paymentMode] +=1
+          }
+          else{
+            modeCounts[data.paymentMode] = 1
+          }
+        }
+      })
+      setVipPay(modeCounts)
+  
+    }
+    const getLB = async () => {
+      const queryLB = await getDocs(collection(db, "LB"));
+      setAllLB(queryLB.size);
+
+      const modes = await getDocs(collection(db, "LB"), where("paymentMode", "!=", ""));
+      const modeCounts = []
+      modes.forEach((snap) => {
+        const data = snap.data();
+        if(data.paymentMode){
+          if(modeCounts[data.paymentMode]){
+            modeCounts[data.paymentMode] +=1
+          }
+          else{
+            modeCounts[data.paymentMode] = 1
+          }
+        }
+      })
+      setLbPay(modeCounts)
+    }
+    const getUPB = async () => {
+      const queryUPB = await getDocs(collection(db, "UPB"));
+      setAllUPB(queryUPB.size)
+
+      const modes = await getDocs(collection(db, "UPB"), where("paymentMode", "!=", ""));
+      const modeCounts = []
+      modes.forEach((snap) => {
+        const data = snap.data();
+        if(data.paymentMode){
+          if(modeCounts[data.paymentMode]){
+            modeCounts[data.paymentMode] +=1
+          }
+          else{
+            modeCounts[data.paymentMode] = 1
+          }
+        }
+      })
+      setUpbPay(modeCounts)
+    }
+    const getAll = async () => {
+    
+        const docRef = doc(db, 'Seat Variations', 'Seat Classes');
+        const docSnap = await getDoc(docRef);
+        const data = docSnap.data();
+        setAllSeats(data)
+    };
+    getAll()
+    getLB()
+    getUPB()
+    getVip()
+  })
+
+  const pieChartData = [
+    { id: 'VIP', value: allSeats.VIP },
+    { id: 'LB', value: allSeats.LB },
+    { id: 'UPB', value: allSeats.UPB },
+  ];
+  
+  
+  const chartData = [
+    {
+      category: "VIP",
+      ...vipPay
+    },
+    {
+      category:"Lower Box",
+      ...lbPay,
+    },
+    {
+      category:"Upper Box",
+      ...upbPay,
+    },
+  ];
   
 
   return (
     <Box m="20px">
          <Header title="DASHBOARD" subtitle="This is where general information are displayed "/>
-       <Box display="flex" justifyContent="space-evenly" >
+        {/* CONTAINS ALL THE RIBBONS OF ALL TICKETS SOLD */}
+       <Box display="flex" justifyContent="space-between" >
          {/* VIP SECTION EME */}
          <Box 
           height="185px"
@@ -48,7 +133,7 @@ const Dashboard = () => {
               borderRadius="20px"
               height="125px"
               position="relative"
-              width="300px"
+              width="325px"
             >
               {/* VIP title */}
                 <Typography
@@ -68,7 +153,7 @@ const Dashboard = () => {
                       display="flex"
                       align="center"
                       sx={{ justifyContent:"center", fontSize: "60px"}}>
-                        999
+                        {allVIP}
                     </Typography>
                     <Typography
                         variant="h2"
@@ -82,65 +167,272 @@ const Dashboard = () => {
               
             </Box>
          </Box>
-         <Box 
+         {/* LOWER BOX */}
+          <Box 
           height="185px"
           weight="455px"
           position="relative"
-          top="100px"
           width="440px">
-          <Box
-            sx={{backgroundColor: colors.grey[200]}}
-            borderRadius="20px"
-            height="150px"
-            position="relative"
-            width="350px"
-          >
-          <Typography
-            variant="h2"
-            color={colors.grey[800]}
-            fontWeight="bold"
-            display="flex"
-            align="center"
-            sx={{m: "5px 0 0 0"}}>
-              hello
-            </Typography>
-          </Box>
-         
+            <Box
+              sx={{backgroundColor: colors.grey[200]}}
+              borderRadius="20px"
+              height="125px"
+              position="relative"
+              width="325px"
+            >
+              {/* title */}
+                <Typography
+                  variant="h2"
+                  color={colors.grey[900]}
+                  fontWeight="bold"
+                  display="flex"
+                  align="center"
+                  sx={{ justifyContent:"center"}}>
+                    Lower Box Section
+                </Typography>
+                <Box display="flex" justifyContent="space-evenly">
+                    <Typography
+                      variant="h1"
+                      color={colors.grey[900]}
+                      fontWeight="1000"
+                      display="flex"
+                      align="center"
+                      sx={{ justifyContent:"center", fontSize: "60px"}}>
+                        {allLB}
+                    </Typography>
+                    <Typography
+                        variant="h2"
+                        color={colors.grey[900]}
+                        display="flex"
+                        align="center"
+                        sx={{ justifyContent:"center", marginTop:"20px"}}>
+                          tickets sold
+                    </Typography>
+                </Box>
+              
+            </Box>
          </Box>
-         <Box 
+         {/* UPPER BOX */}
+          <Box 
           height="185px"
           weight="455px"
           position="relative"
-          top="100px"
           width="440px">
-          <Box
-            sx={{backgroundColor: colors.grey[200]}}
-            borderRadius="20px"
-            height="150px"
-            position="relative"
-            width="350px"
-          >
-          <Typography
-            variant="h2"
-            color={colors.grey[800]}
-            fontWeight="bold"
-            display="flex"
-            align="center"
-            sx={{m: "5px 0 0 0"}}>
-              hello
-            </Typography>
-          </Box>
-         
+            <Box
+              sx={{backgroundColor: colors.grey[200]}}
+              borderRadius="20px"
+              height="125px"
+              position="relative"
+              width="325px"
+            >
+              {/*  title */}
+                <Typography
+                  variant="h2"
+                  color={colors.grey[900]}
+                  fontWeight="bold"
+                  display="flex"
+                  align="center"
+                  sx={{ justifyContent:"center"}}>
+                    Upper Box Section
+                </Typography>
+                <Box display="flex" justifyContent="space-evenly">
+                    <Typography
+                      variant="h1"
+                      color={colors.grey[900]}
+                      fontWeight="1000"
+                      display="flex"
+                      align="center"
+                      sx={{ justifyContent:"center", fontSize: "60px"}}>
+                        {allUPB}
+                    </Typography>
+                    <Typography
+                        variant="h2"
+                        color={colors.grey[900]}
+                        display="flex"
+                        align="center"
+                        sx={{ justifyContent:"center", marginTop:"20px"}}>
+                          tickets sold
+                    </Typography>
+                </Box>
+              
+            </Box>
          </Box>
-          {/* <Typography
-            variant="h2"
-            color={colors.grey[100]}
-            fontWeight="bold"
-            sx={{m: "5px 0 0 0"}}>
-            </Typography> */}
-
        </Box>
-   
+
+      {/* THE GRAPHS */}
+      <Box display="flex" justifyContent="space-between">
+        <Box 
+            height="185px"
+            weight="455px"
+            position="relative"
+            width="440px">
+              <Box
+                sx={{backgroundColor: colors.grey[200]}}
+                borderRadius="20px"
+                height="375px"
+                position="relative"
+                width="620px"
+              >
+                <Typography
+                  variant="h2"
+                  color={colors.grey[900]}
+                  fontWeight="bold"
+                  display="flex"
+                  align="center"
+                  sx={{ justifyContent:"center"}}>
+                    Payment Modes
+                </Typography>
+               <ResponsiveBar
+                  data={chartData}
+                  keys={[...new Set(chartData.flatMap(item => Object.keys(item)))].filter(key => key !== 'category')}
+                  indexBy="category"
+                  margin={{ top: 20, right: 130, bottom: 70, left: 60 }}
+                  padding={0.3}
+                  valueScale={{ type: 'linear' }}
+                  indexScale={{ type: 'band', round: true }}
+                  colors={{ scheme: 'nivo' }}
+                  isInteractive={false}
+                  axisLeft={{
+                      tickSize: 2,
+                      tickPadding: 2,
+                      tickRotation: 0,
+                      legend: 'Ticket numbers',
+                      legendPosition: 'middle',
+                      legendOffset: -40,
+                      truncateTickAt: 0,
+                  }}
+                  labelSkipWidth={12}
+                  labelSkipHeight={12}
+                  labelTextColor={{
+                      from: 'color',
+                      modifiers: [
+                          [
+                              'darker',
+                              10
+                          ]
+                      ]
+                  }}
+                  
+              legends={[
+                  {
+                      dataFrom: 'keys',
+                      anchor: 'right',
+                      direction: 'column',
+                      justify: false,
+                      translateX: 120,
+                      translateY: 0,
+                      itemsSpacing: 2,
+                      itemWidth: 100,
+                      itemHeight: 20,
+                      itemDirection: 'left-to-right',
+                      itemOpacity: 0.85,
+                      symbolSize: 20,
+                      effects: [
+                          {
+                              on: 'hover',
+                              style: {
+                                  itemOpacity: 1
+                              }
+                          }
+                      ]
+                  },
+              ]}
+           />
+            </Box>
+        </Box>   
+              <Box
+                sx={{backgroundColor: colors.grey[200]}}
+                borderRadius="20px"
+                height="375px"
+                position="relative"
+                width="620px"
+              >
+                <Typography
+                  variant="h2"
+                  color={colors.grey[900]}
+                  fontWeight="bold"
+                  display="flex"
+                  align="center"
+                  sx={{ justifyContent:"center"}}>
+                    Seats Remaining
+                </Typography>
+                 <ResponsivePie
+                    data={pieChartData}
+                    isInteractive={false}
+                    margin={{ top: 40, right: 80, bottom: 80, left: 80 }}
+                    innerRadius={0.5}
+                    colors={{scheme: "nivo"}}
+                    padAngle={0.7}
+                    cornerRadius={3}
+                    activeOuterRadiusOffset={8}
+                    borderWidth={1}
+                    borderColor={{
+                        from: 'color',
+                        modifiers: [
+                            [
+                                'darker',
+                                0.75
+                            ]
+                        ]
+                    }}
+                      arcLinkLabelsSkipAngle={10}
+                      arcLinkLabelsTextColor="#333333"
+                      arcLinkLabelsThickness={2}
+                      arcLinkLabelsColor={{ from: 'color' }}
+                      arcLabelsSkipAngle={10}
+                      arcLabelsTextColor={{
+                          from: 'color',
+                          modifiers: [
+                              [
+                                  'darker',
+                                  10
+                              ]
+                          ]
+                      }}
+                      defs={[
+                          {
+                              id: 'dots',
+                              type: 'patternDots',
+                              background: 'inherit',
+                              color: 'rgba(255, 255, 255, 0.3)',
+                              size: 4,
+                              padding: 1,
+                              stagger: true
+                          },
+                          {
+                              id: 'lines',
+                              type: 'patternLines',
+                              background: 'inherit',
+                              color: 'rgba(255, 255, 255, 0.3)',
+                              rotation: -45,
+                              lineWidth: 6,
+                              spacing: 10
+                          }
+                      ]}
+                      fill={[
+                          {
+                              match: {
+                                  id: 'VIP'
+                              },
+                              id: 'dots'
+                          },
+                          {
+                              match: {
+                                  id: 'LB'
+                              },
+                              id: 'lines'
+                          },
+                          {
+                              match: {
+                                  id: 'UPB'
+                              },
+                              id: 'dots'
+                          },
+                      ]}
+                   
+                    />
+                </Box>
+      </Box>
     </Box>
   )
 }
